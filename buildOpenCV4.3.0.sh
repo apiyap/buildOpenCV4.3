b4b18ab9488af0764a893bc2de59d9fc1c588299
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-06-02 10:10:09 +0700

    Update buildOpenCV4.3.0.sh

--- buildOpenCV4.3.0.sh
+++ buildOpenCV4.3.0.sh
@@ -4,7 +4,7 @@
 
 OPENCV_VERSION=4.3.0
 # Jetson Nano
-ARCH_BIN="5.3,6.2,7.2"
+ARCH_BIN="5.3"
 INSTALL_DIR=/usr/local
 # Download the opencv_extras repository
 # If you are installing the opencv testdata, ie
